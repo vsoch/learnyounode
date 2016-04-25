e20b546cdc27dfff97c5425633012eaa462ec12c
vsoch <vsochat@stanford.edu>
2016-04-24 23:48:15 -0700

    adding elctron app

---
+++ electron/index.js
@@ -0,0 +1,24 @@
+var picture = require('cat-picture')
+var fs = require('fs');
+
+var src = picture.src
+picture.remove()
+
+var image = require('lightning-image-poly')
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
+
+var remote = require('electron').remote
+
+function save () {
+  remote.getCurrentWindow().webContents.printToPDF({
+    portrait: true
+  }, function (err, data) {
+    fs.writeFile('annotation.pdf', data, function (err) {
+      if (err) alert('error generating pdf! ' + err.message)
+      else alert('pdf saved!')
+    })
+  })  
+}
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode == 80) save()
+})
